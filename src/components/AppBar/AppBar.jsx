eddd020cef47e4eda99dcec479b115663159dffa
AnastasiiaHudymenko <106466791+AnastasiiaHudymenko@users.noreply.github.com>
2023-07-28 16:38:36 +0300

    fix

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -4,9 +4,9 @@ import logo from "../../images/logo.png";
 const AppBar = () => {
   return (
     <Header>
-      <div>
+      <>
         <img src={logo} alt="logo app" />
-      </div>
+      </>
     </Header>
   );
 };
