d2ca1e3146cb92ca56b82fe6bbdbc8f1244a876e
AnastasiiaHudymenko <106466791+AnastasiiaHudymenko@users.noreply.github.com>
2023-08-26 08:59:46 +0300

    add custom select

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -3,20 +3,33 @@ import DatePicker from "react-datepicker";
 import { format } from "date-fns";
 import { BsCalendar4 } from "react-icons/bs";
 import { BiSolidDownArrow } from "react-icons/bi";
+import { Formik, Form } from "formik";
 import "react-datepicker/dist/react-datepicker.css";
 import {
   WrapCalendarInput,
   TitleInputCalendar,
   WrapContent,
-  ContainerMain,
+  ContainerMainForm,
   WrapCalendarPosition,
+  WrapSelect,
+  TitleSelect,
+  ListSelect,
+  TitleList,
+  ItemSelect,
 } from "./Calendar.styled";
+import { useSelector } from "react-redux";
 
 const Calendar = () => {
   const [openStartCalendar, setOpenStartCalendar] = useState(false);
   const [openEndCalendar, setOpenEndCalendar] = useState(false);
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
+  const [openSelect, setOpenSelect] = useState(false);
+  const [selectBook, setSelectBook] = useState(null);
+  const [selectIdBook, setSelectIdBook] = useState(null);
+  const books = useSelector((state) => state.userBook?.books?.goingToRead);
+
+  console.log(books);
 
   const handleClickStart = () => {
     if (startDate) {
@@ -29,56 +42,88 @@ const Calendar = () => {
     if (endDate) {
       setEndDate(null);
     }
-
     setOpenEndCalendar(true);
   };
+
+  const handleClickSelect = () => {
+    if (selectBook) {
+      setSelectBook(null);
+    }
+    setOpenSelect(true);
+  };
+
+  const handleClickBook = (book) => {
+    setSelectBook(book.title);
+    setSelectIdBook(book._id);
+  };
   return (
-    <ContainerMain>
-      <WrapCalendarInput onClick={handleClickStart}>
-        <WrapContent>
-          <BsCalendar4 size={20} fill="#a6abb9" />
-          <TitleInputCalendar>
-            {!startDate ? "Початок" : startDate}
-          </TitleInputCalendar>
-        </WrapContent>
+    <Formik>
+      <ContainerMainForm>
+        <WrapCalendarInput onClick={handleClickStart}>
+          <WrapContent>
+            <BsCalendar4 size={20} fill="#a6abb9" />
+            <TitleInputCalendar>
+              {!startDate ? "Початок" : startDate}
+            </TitleInputCalendar>
+          </WrapContent>
 
-        <BiSolidDownArrow />
-        {openStartCalendar && !startDate && (
-          <WrapCalendarPosition>
-            <DatePicker
-              selected={startDate}
-              onChange={(date) => {
-                setStartDate(format(new Date(date), "yyyy-MM-dd"));
-              }}
-              inline
-              minDate={new Date()}
-            />
-          </WrapCalendarPosition>
-        )}
-      </WrapCalendarInput>
+          <BiSolidDownArrow />
+          {openStartCalendar && !startDate && (
+            <WrapCalendarPosition>
+              <DatePicker
+                selected={startDate}
+                onChange={(date) => {
+                  setStartDate(format(new Date(date), "yyyy-MM-dd"));
+                }}
+                inline
+                minDate={new Date()}
+              />
+            </WrapCalendarPosition>
+          )}
+        </WrapCalendarInput>
 
-      <WrapCalendarInput onClick={handleClickEnd}>
-        <WrapContent>
-          <BsCalendar4 size={20} fill="#a6abb9" />
-          <TitleInputCalendar>
-            {!endDate ? "Завершення" : endDate}
-          </TitleInputCalendar>
-        </WrapContent>
+        <WrapCalendarInput onClick={handleClickEnd}>
+          <WrapContent>
+            <BsCalendar4 size={20} fill="#a6abb9" />
+            <TitleInputCalendar>
+              {!endDate ? "Завершення" : endDate}
+            </TitleInputCalendar>
+          </WrapContent>
 
-        <BiSolidDownArrow />
-        {openEndCalendar && !endDate && (
-          <WrapCalendarPosition>
-            <DatePicker
-              selected={endDate}
-              onChange={(date) =>
-                setEndDate(format(new Date(date), "yyyy-MM-dd"))
-              }
-              inline
-            />
-          </WrapCalendarPosition>
-        )}
-      </WrapCalendarInput>
-    </ContainerMain>
+          <BiSolidDownArrow />
+          {openEndCalendar && !endDate && (
+            <WrapCalendarPosition>
+              <DatePicker
+                selected={endDate}
+                onChange={(date) =>
+                  setEndDate(format(new Date(date), "yyyy-MM-dd"))
+                }
+                inline
+                minDate={new Date()}
+              />
+            </WrapCalendarPosition>
+          )}
+        </WrapCalendarInput>
+        <WrapSelect onClick={handleClickSelect}>
+          <TitleSelect>
+            {!selectBook ? " Обрати книгу з біббліотеки" : selectBook}
+          </TitleSelect>
+          <BiSolidDownArrow />
+          {openSelect && !selectBook && (
+            <ListSelect>
+              {books?.map((book) => (
+                <ItemSelect
+                  onClick={() => handleClickBook(book)}
+                  key={book._id}
+                >
+                  <TitleList>{book.title}</TitleList>
+                </ItemSelect>
+              ))}
+            </ListSelect>
+          )}
+        </WrapSelect>
+      </ContainerMainForm>
+    </Formik>
   );
 };
 
--- src/components/Calendar/Calendar.styled.jsx
+++ src/components/Calendar/Calendar.styled.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
+import { Form } from "formik";
 
-export const ContainerMain = styled.div`
+export const ContainerMainForm = styled(Form)`
   display: flex;
   align-items: center;
   justify-content: center;
@@ -25,6 +26,7 @@ export const WrapCalendarPosition = styled.div`
   position: absolute;
   z-index: 999;
   top: 0;
+  right: 0;
 `;
 
 export const TitleInputCalendar = styled.p`
@@ -37,4 +39,52 @@ export const TitleInputCalendar = styled.p`
 export const WrapContent = styled.div`
   display: flex;
   gap: 5px;
+`;
+
+export const WrapSelect = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 280px;
+  height: 42px;
+  padding: 0 5px;
+  background: #fff;
+  box-shadow: 0px 1px 2px 0px rgba(29, 29, 27, 0.15) inset;
+`;
+
+export const TitleSelect = styled.p`
+  color: #a6abb9;
+  font-size: 14px;
+  font-weight: 500;
+  margin: 0;
+`;
+
+export const ListSelect = styled.ul`
+  position: absolute;
+  z-index: 999;
+  display: flex;
+  border-radius: 5px;
+  flex-direction: column;
+  background: #fff;
+  width: 100%;
+  top: 44px;
+  box-shadow: 0px 1px 2px 0px rgba(29, 29, 27, 0.15) inset;
+  left: 0;
+`;
+
+export const ItemSelect = styled.li`
+  border-bottom: 1px solid #cccccc29;
+  padding: 8px 5px;
+  transition: background-color 0.3s ease;
+  &:hover {
+    background-color: #cccccc38;
+  }
+`;
+
+export const TitleList = styled.p`
+  color: #a6abb9;
+  font-size: 14px;
+  font-weight: 500;
+  margin: 0;
 `;
